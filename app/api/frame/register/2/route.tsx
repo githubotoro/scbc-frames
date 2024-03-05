1e9af62b7105152ac94382e8f08c20193e38b08e
githubotoro <yupuday.git@gmail.com>
2024-03-05 14:02:04 -0800

    first commit

---
+++ app/api/frame/register/2/route.tsx
@@ -0,0 +1,68 @@
+import { FrameRequest, getFrameHtmlResponse } from "@coinbase/onchainkit/frame";
+import { NextRequest, NextResponse } from "next/server";
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  const body: FrameRequest = await req.json();
+
+  const buttonIndex: number = body.untrustedData.buttonIndex;
+
+  if (buttonIndex === 1) {
+    return new NextResponse(
+      getFrameHtmlResponse({
+        buttons: [
+          {
+            action: "link",
+            label: "Register",
+            target: "https://lu.ma/7atw9xr0",
+          },
+          {
+            label: "Headline Speakers",
+            action: "post",
+          },
+        ],
+        image: {
+          src: `${process.env.NEXT_PUBLIC_URL}/frame/register/1.png`,
+          aspectRatio: "1:1",
+        },
+        postUrl: `${process.env.NEXT_PUBLIC_URL}/api/frame/register/1`,
+      }),
+      {
+        headers: {
+          "Cache-Control": "max-age=3600", // refresh image every hour
+        },
+      }
+    );
+  } else {
+    return new NextResponse(
+      getFrameHtmlResponse({
+        buttons: [
+          {
+            label: "<<",
+            action: "post",
+          },
+          {
+            action: "link",
+            label: "Register",
+            target: "https://lu.ma/7atw9xr0",
+          },
+        ],
+        image: {
+          src: `${process.env.NEXT_PUBLIC_URL}/frame/register/3.png`,
+          aspectRatio: "1:1",
+        },
+        postUrl: `${process.env.NEXT_PUBLIC_URL}/api/frame/register/3`,
+      }),
+      {
+        headers: {
+          "Cache-Control": "max-age=3600", // refresh image every hour
+        },
+      }
+    );
+  }
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export const dynamic = "force-dynamic";
---
+++ app/register/page.tsx
@@ -0,0 +1,41 @@
+import { getFrameMetadata } from "@coinbase/onchainkit/frame";
+import type { Metadata } from "next";
+import { redirect } from "next/navigation";
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      action: "link",
+      label: "Register",
+      target: "https://lu.ma/7atw9xr0",
+    },
+    {
+      label: "Headline Speakers",
+      action: "post",
+    },
+  ],
+  image: {
+    src: `${process.env.NEXT_PUBLIC_URL}/frame/register/1.png`,
+    aspectRatio: "1:1",
+  },
+  postUrl: `${process.env.NEXT_PUBLIC_URL}/api/frame/register/1`,
+});
+
+export const metadata: Metadata = {
+  title: "Southern California Blockchain Conference",
+  description:
+    "University of Southern California presents an unrivaled opportunity for the brightest students and industry leaders to gather for the purpose of illuminating the bright future ahead for digital assets and blockchain technology. Join us at our annual 2-day marquee event to uncover emerging trends, explore cutting-edge research, and celebrate our community of builders.",
+  openGraph: {
+    title: "Southern California Blockchain Conference",
+    description:
+      "University of Southern California presents an unrivaled opportunity for the brightest students and industry leaders to gather for the purpose of illuminating the bright future ahead for digital assets and blockchain technology. Join us at our annual 2-day marquee event to uncover emerging trends, explore cutting-edge research, and celebrate our community of builders.",
+    images: [`${process.env.NEXT_PUBLIC_URL}/register/frame/1.png`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
+
+export default function Page() {
+  redirect("https://lu.ma/7atw9xr0");
+}
